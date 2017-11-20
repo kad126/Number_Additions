58572c10950cbe0032912e67de0964872a54d145
Khadija Al-Selini <33031527+kad126@users.noreply.github.com>
2017-11-20 20:56:01 +0000

    Update Number_addition_3.java

--- Number_addition_3.java
+++ Number_addition_3.java
@@ -1,4 +1,4 @@
-//This is a program that asks the user for a number n and gives them the possibility to choose
+//This is a program that asks the user for a number, n, and gives them the possibility to choose
 //between computing the sum and computing the product of 1…n.
 	
 import java.util.Scanner;
